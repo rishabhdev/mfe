903878b7cb533e8e45e9888e846d8463e4fdd845
rishabhdev <rishabdev919@gmail.com>
2024-10-22 15:37:19 +0530

    randomize classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { mount } from 'marketing/MarketingApp';
-import { BrowserRouter } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 export default () => {
   return (
     <BrowserRouter>
-    <div>
-      {/* <h1>Hi there!!!!</h1> */}
-      <hr />
-      <Header />
-      <MarketingApp />
-    </div>
+        <StylesProvider generateClassName={generateClassName}>
+
+      <div>
+        {/* <h1>Hi there!!!!</h1> */}
+        <hr />
+        <Header />
+        <MarketingApp />
+      </div>
+      </StylesProvider>
+
     </BrowserRouter>
   );
-}
+};
